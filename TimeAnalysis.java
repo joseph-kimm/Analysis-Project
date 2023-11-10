983c80bda081aae7a193f132bb699eb9eba5abdd
kalte0 <37700194+kalte0@users.noreply.github.com>
2023-11-10 17:24:44 -0500

    Working Time Analysis

--- TimeAnalysis.java
+++ TimeAnalysis.java
@@ -1,10 +1,10 @@
 public class TimeAnalysis { // number of times to run because each test. 
-    final int numRuns = 10; // the number of times to run and average each time. 
+    final int numRuns = 5; // the number of times to run and average each time. 
     final int[] numStudents = {10, 25, 50, 100, 250, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
     public static void main(String[] args) {
         TimeAnalysis ta = new TimeAnalysis(); 
          // JIT runs:  
-        for (int i = 0; i < ta.numRuns; i ++ ) { 
+        for (int i = 1; i < ta.numRuns; i ++ ) { 
            ScheduleMaker sm = new ScheduleMaker(new String("demo_files/10Students/s10-" + i + "_const.txt"), new String("demo_files/10Students/s10-" + i + "_pref.txt"));
            System.out.println(sm.getNanoSecondsElapsed());
         }
@@ -16,8 +16,6 @@ public class TimeAnalysis { // number of times to run because each test.
     }
 
     public void printStudentTimes(int s) {
-       
-    
         long nanoSecondsElapsed = 0; 
         for (int i = 1; i < 5; i++ ) { // for each randomized demo file
             ScheduleMaker sm = new ScheduleMaker(new String("demo_files/" + s + "Students/s" + s + "-" + i + "_const.txt"), new String("demo_files/" + s + "Students/s" + s + "-" + i + "_pref.txt"));
