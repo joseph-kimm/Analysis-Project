4603b758270e0b9009fa956bc3b5657b85ca9281
kalte0 <37700194+kalte0@users.noreply.github.com>
2023-11-11 10:15:14 -0500

    Time Analysis File and Data Generation file complete for variation in s and c

--- MakeDemoFiles.java
+++ MakeDemoFiles.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.File; 
 
 public class MakeDemoFiles {
-    final int[] numStudents = {10, 25, 50, 100, 250, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
+    final int[] X = {10, 50, 100, 250, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
 
     final int r = 5;
     final int c = 20;
@@ -18,7 +18,7 @@ public class MakeDemoFiles {
     public void makeStudentFiles() { 
         System.out.println("");
         try { 
-            for (int s : numStudents) { 
+            for (int s : X) { 
                 for (int i = 1; i <= 5; i ++ ) { 
                     ProcessBuilder pb = new ProcessBuilder("Perl", 
                                                         "make_random_input.pl",
@@ -35,6 +35,33 @@ public class MakeDemoFiles {
                     Process p = pb.start(); 
                 }
             }
+            for (int c : X) { 
+                ProcessBuilder mkBuilder = new ProcessBuilder("mkdir", 
+                                                        "demo_files/" + c + "Classes",
+                                                        "demo_files/" + c + "Classes"
+                                                        );
+                mkBuilder.redirectErrorStream(true);
+                File mklog = new File("log.txt"); 
+                mkBuilder.redirectOutput(mklog);
+                mkBuilder.start(); 
+            
+                for (int i = 1; i <= 5; i ++ ) { 
+                    ProcessBuilder pb = new ProcessBuilder("Perl", 
+                                                        "make_random_input.pl",
+                                                        Integer.toString(c/10), 
+                                                        Integer.toString(c), 
+                                                        Integer.toString(t),
+                                                        Integer.toString(1000),
+                                                        "demo_files/" + c + "Classes/c" + c + "-" + i + "_const.txt",
+                                                        "demo_files/" + c + "Classes/c" + c + "-" + i + "_pref.txt"
+                                                        );
+                    pb.redirectErrorStream(true);
+                    File log = new File("log.txt"); 
+                    pb.redirectOutput(log);
+                    Process p = pb.start(); 
+                }
+            }
+            
         }
         catch (IOException ioe) { 
             System.err.println("IO Exception" + ioe);
--- TimeAnalysis.java
+++ TimeAnalysis.java
@@ -1,18 +1,46 @@
 public class TimeAnalysis { // number of times to run because each test. 
     final int numRuns = 5; // the number of times to run and average each time. 
-    final int[] numStudents = {10, 25, 50, 100, 250, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
+    final int[] X = {10, 50, 100, 250, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000};
     public static void main(String[] args) {
         TimeAnalysis ta = new TimeAnalysis(); 
-         // JIT runs:  
-        for (int i = 1; i <= ta.numRuns; i ++ ) { 
-           ScheduleMaker sm = new ScheduleMaker(new String("demo_files/10Students/s10-" + i + "_const.txt"), new String("demo_files/10Students/s10-" + i + "_pref.txt"));
-           System.out.println(sm.getNanoSecondsElapsed());
-        }
+        if (args.length == 1) { 
+            if (args[0].equals("s")) { 
+                 // JIT runs:  
+                for (int i = 1; i <= ta.numRuns; i ++ ) { 
+                    ScheduleMaker sm = new ScheduleMaker(new String("demo_files/10Students/s10-" + i + "_const.txt"), new String("demo_files/10Students/s10-" + i + "_pref.txt"));
+                    System.out.println(sm.getNanoSecondsElapsed());
+                }
 
-        System.out.print("s\tavgMicroSeconds\n");
-        for (int s : ta.numStudents) { 
-            ta.printStudentTimes(s); 
+                System.out.print("s\tavgMicroSeconds\n");
+                for (int s : ta.X) { 
+                    ta.printStudentTimes(s); 
+                }
+            }
+            if (args[0].equals("c")) { 
+                 // JIT runs:  
+                for (int i = 1; i <= ta.numRuns; i ++ ) { 
+                    ScheduleMaker sm = new ScheduleMaker(new String("demo_files/10Classes/c10-" + i + "_const.txt"), new String("demo_files/10Classes/c10-" + i + "_pref.txt"));
+                    System.out.println(sm.getNanoSecondsElapsed());
+                }
+
+                System.out.print("s\tavgMicroSeconds\n");
+                for (int c : ta.X) { 
+                    ta.printClassTimes(c); 
+                }
+            }
         }
+        // JIT runs:  
+                for (int i = 1; i <= ta.numRuns; i ++ ) { 
+                    ScheduleMaker sm = new ScheduleMaker(new String("demo_files/10Classes/c10-" + i + "_const.txt"), new String("demo_files/10Classes/c10-" + i + "_pref.txt"));
+                    System.out.println(sm.getNanoSecondsElapsed());
+                }
+
+                System.out.print("c\tavgMicroSeconds\n");
+                for (int c : ta.X) { 
+                    ta.printClassTimes(c); 
+                }
+        
+        
     }
 
     public void printStudentTimes(int s) {
@@ -23,5 +51,15 @@ public class TimeAnalysis { // number of times to run because each test.
         }
         long averageMicroSecondsElapsed = (nanoSecondsElapsed / 1000) / 5;
         System.out.println(s + "\t" + averageMicroSecondsElapsed); 
+    }
+
+    public void printClassTimes(int c) { 
+        long nanoSecondsElapsed = 0; 
+        for (int i = 1; i <= 5; i++ ) { // for each randomized demo file
+            ScheduleMaker sm = new ScheduleMaker(new String("demo_files/" + c + "Classes/c" + c + "-" + i + "_const.txt"), new String("demo_files/" + c + "Classes/c" + c + "-" + i + "_pref.txt"));
+            nanoSecondsElapsed += sm.getNanoSecondsElapsed();
+        }
+        long averageMicroSecondsElapsed = (nanoSecondsElapsed / 1000) / 5;
+        System.out.println(c + "\t" + averageMicroSecondsElapsed); 
     }
 }
